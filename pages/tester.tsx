ebbf03712c6685fa430fa901166515ddd8cdfbb5
anhquang2605 <anhquang2605@gmail.com>
2023-12-02 10:18:06 -0800

    set up pages routes for the app

---
+++ pages/tester.tsx
@@ -0,0 +1,10 @@
+export default function Tester() {
+    return(
+        <main>
+            <h1>Tester</h1>
+            <p>
+                This is a test page.    
+            </p>
+        </main>
+    )
+}
